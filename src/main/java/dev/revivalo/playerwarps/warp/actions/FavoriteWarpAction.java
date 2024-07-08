f00ff1d87c341bfbb77594f39c1b1ea892136587
Adam Bíba <revivalo@athelion.eu>
2024-07-08 21:23:41 +0200

    Tweaked favouring

--- src/main/java/dev/revivalo/playerwarps/warp/actions/FavoriteWarpAction.java
+++ src/main/java/dev/revivalo/playerwarps/warp/actions/FavoriteWarpAction.java
@@ -7,6 +7,7 @@ import dev.revivalo.playerwarps.warp.Warp;
 import dev.revivalo.playerwarps.warp.WarpAction;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FavoriteWarpAction implements WarpAction<Void> {
@@ -14,19 +15,22 @@ public class FavoriteWarpAction implements WarpAction<Void> {
     public boolean execute(Player player, Warp warp, Void data) {
         final PlayerConfig playerData = PlayerConfig.getConfig(player);
         final List<String> favorites = playerData.getStringList("favorites");
-        if (!favorites.contains(warp.toString())) {
-            favorites.add(warp.toString());
-            playerData.set("favorites", favorites);
-            player.sendMessage(Lang.ADD_FAVORITE.asColoredString().replace("%warp%", warp.getName()));
-        } else {
-            favorites.remove(warp.toString());
-            playerData.set("favorites", favorites);
+        final String warpString = warp.toString();
+
+        if (favorites.contains(warpString)) {
+            favorites.remove(warpString);
             player.sendMessage(Lang.REMOVE_FAVORITE.asColoredString().replace("%warp%", warp.getName()));
+        } else {
+            favorites.add(warpString);
+            player.sendMessage(Lang.ADD_FAVORITE.asColoredString().replace("%warp%", warp.getName()));
         }
+
+        playerData.set("favorites", favorites);
         playerData.save();
 
         return true;
     }
+
 
     @Override
     public PermissionUtils.Permission getPermission() {
