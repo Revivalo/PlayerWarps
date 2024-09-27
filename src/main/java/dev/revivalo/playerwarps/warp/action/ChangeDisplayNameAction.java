cadf6b4a8f4dcc3145a6361c79635ae2e8aeb620
Adam Bíba <revivalo@athelion.eu>
2024-09-27 19:21:46 +0200

    Text length now should ignore color codes

--- src/main/java/dev/revivalo/playerwarps/warp/action/ChangeDisplayNameAction.java
+++ src/main/java/dev/revivalo/playerwarps/warp/action/ChangeDisplayNameAction.java
@@ -3,6 +3,7 @@ package dev.revivalo.playerwarps.warp.action;
 import dev.revivalo.playerwarps.configuration.file.Config;
 import dev.revivalo.playerwarps.configuration.file.Lang;
 import dev.revivalo.playerwarps.util.PermissionUtil;
+import dev.revivalo.playerwarps.util.TextUtil;
 import dev.revivalo.playerwarps.warp.Warp;
 import dev.revivalo.playerwarps.warp.WarpAction;
 import org.bukkit.entity.Player;
@@ -12,11 +13,13 @@ import java.util.HashMap;
 public class ChangeDisplayNameAction implements WarpAction<String> {
     @Override
     public boolean execute(Player player, Warp warp, String text) {
-        int textLength = text.length();
+        int textLength = TextUtil.stripColor(text).length();
         if (textLength < 3 || textLength > 32) {
             player.sendMessage(Lang.TEXT_SIZE_ERROR.asColoredString());
             return false;
         }
+
+
         warp.setDisplayName(text);
         player.sendMessage(Lang.DISPLAY_NAME_CHANGED.asReplacedString(player, new HashMap<String, String>() {{
             put("%warp%", warp.getName());
--- src/main/java/dev/revivalo/playerwarps/warp/action/SetDescriptionAction.java
+++ src/main/java/dev/revivalo/playerwarps/warp/action/SetDescriptionAction.java
@@ -3,6 +3,7 @@ package dev.revivalo.playerwarps.warp.action;
 import dev.revivalo.playerwarps.configuration.file.Config;
 import dev.revivalo.playerwarps.configuration.file.Lang;
 import dev.revivalo.playerwarps.util.PermissionUtil;
+import dev.revivalo.playerwarps.util.TextUtil;
 import dev.revivalo.playerwarps.warp.Warp;
 import dev.revivalo.playerwarps.warp.WarpAction;
 import org.bukkit.entity.Player;
@@ -12,7 +13,7 @@ import java.util.HashMap;
 public class SetDescriptionAction implements WarpAction<String> {
     @Override
     public boolean execute(Player player, Warp warp, String text) {
-        int textLength = text.length();
+        int textLength = TextUtil.stripColor(text).length();
         if (textLength < 5 || textLength > Config.SET_DESCRIPTION_CHARACTERS_LIMIT.asInteger()) {
             player.sendMessage(Lang.TEXT_SIZE_ERROR.asColoredString().replace("%limit%", Config.SET_DESCRIPTION_CHARACTERS_LIMIT.asString()));
             return false;
